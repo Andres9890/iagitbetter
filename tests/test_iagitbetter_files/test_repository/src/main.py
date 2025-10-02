d5935cc93cf8f8b8e199ebd763ad3966c7bfc3f2
Andres99 <veryshort8@gmail.com>
2025-10-02 21:31:33 +0300

    Update main.py

--- tests/test_iagitbetter_files/test_repository/src/main.py
+++ tests/test_iagitbetter_files/test_repository/src/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-  """Test main file"""
-  
-  def main():
-      print("Where am i, is anyone there?")
-      return 0
-  
-  if __name__ == "__main__":
-      exit(main())
+"""Test main file"""
+
+def main():
+    print("Where am i, is anyone there?")
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
